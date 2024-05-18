6b47b011f15f8178163e58ec7c0d49a9ee60de2b
sangatsharma <sangatsharma2@gmail.com>
2024-05-18 23:11:51 +0545

    ..

--- server.js
+++ server.js
@@ -91,7 +91,3 @@ app.get("/", (req, res) => {
 });
 
 module.exports = app;
-module.exports = {
-  // Changes the cache location for Puppeteer.
-  cacheDirectory: path.join(__dirname, '.cache', 'puppeteer'),
-};
