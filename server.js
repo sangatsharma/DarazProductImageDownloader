573a46739e16304be5a6967d2e4c77199af51141
sangatsharma <sangatsharma2@gmail.com>
2024-05-19 13:51:01 +0545

    fix

--- server.js
+++ server.js
@@ -86,11 +86,9 @@ const PORT = process.env.PORT || 5500;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./index.html"));
 });
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./index.html"));
-// });
 
 module.exports = app;
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,3 @@
- preflightContinue: false, //
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./index.html"));
+});
